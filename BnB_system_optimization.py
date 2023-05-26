de0f71f10ee752021319f936211f7cc0cddf6ab1
Golden-Slumber <38204089+Golden-Slumber@users.noreply.github.com>
2023-05-26 17:55:25 +0800

    Update BnB_system_optimization.py

--- BnB_system_optimization.py
+++ BnB_system_optimization.py
@@ -34,7 +34,8 @@ class Node(object):
         return self.upper_bound
 
 
-def partial_subcarrier_optimization(idx, subcarrier_set, tau_mat, h_mat, alpha_mat, square_alpha_mat, pre_indicator_mat,
+def partial_subcarrier_optimization(idx, subcarrier_set, tau_mat, h_mat, stored_value_mat, stored_sqaure_value_mat,
+                                    pre_indicator_mat,
                                     eta=None,
                                     max_iter=50):
     N, J = tau_mat.shape
@@ -49,9 +50,13 @@ def partial_subcarrier_optimization(idx, subcarrier_set, tau_mat, h_mat, alpha_m
             indicator_mat[j, k] = 1 if k == subcarrier_list[i] else 0
     if eta is None:
         # eta = 0.27
-        eta = 0.2
+        eta = 0.01
     else:
         eta = eta
+
+    value_vec = numpy.zeros(K)
+    sub_value_vec = numpy.zeros(K)
+    scale_value_vec = numpy.zeros(K)
 
     for it in range(max_iter):
         # multipler update
@@ -63,14 +68,9 @@ def partial_subcarrier_optimization(idx, subcarrier_set, tau_mat, h_mat, alpha_m
         # indicator update
         vis_idx_list = list()
         for j in range(idx, J):
-            value_vec = numpy.zeros(K)
-            sub_value_vec = numpy.zeros(K)
-            scale_value_vec = numpy.zeros(K)
             for k in subcarrier_list:
-                sub_value_vec[k] = lambda_vec[k] - 2 * numpy.sum(numpy.multiply(tau_mat[:, j], alpha_mat[:, j, k]))
-                scale_value_vec[k] = 2 * numpy.sum(numpy.multiply(tau_mat[:, j], square_alpha_mat[:, j, k]))
-                value_vec[k] = lambda_vec[k] - 2 * numpy.sum(numpy.multiply(tau_mat[:, j], alpha_mat[:, j, k])) + \
-                               scale_value_vec[k]
+                sub_value_vec[k] = lambda_vec[k] - stored_value_mat[j, k]
+                value_vec[k] = lambda_vec[k] - stored_value_mat[j, k] + stored_sqaure_value_mat[j, k]
 
             inner_idx = numpy.argmin(value_vec)
             sub_value_list = list()
@@ -83,13 +83,13 @@ def partial_subcarrier_optimization(idx, subcarrier_set, tau_mat, h_mat, alpha_m
                 for k in subcarrier_list:
                     indicator_mat[j, k] = 1 if k == inner_idx else 0
             else:
-                left = numpy.max(value_vec) - numpy.max(scale_value_vec)
-                right = numpy.max(value_vec) + numpy.max(scale_value_vec)
+                left = numpy.max(value_vec) - numpy.max(stored_sqaure_value_mat[j])
+                right = numpy.max(value_vec) + numpy.max(stored_sqaure_value_mat[j])
                 for binary_it in range(20):
                     mu_vec[j] = (left + right) / 2
                     for k in subcarrier_list:
                         indicator_mat[j, k] = 0 if sub_value_vec[k] >= mu_vec[j] else (mu_vec[j] - sub_value_vec[k]) / \
-                                                                                      scale_value_vec[k]
+                                                                                      stored_sqaure_value_mat[j, k]
                         tmp = numpy.sum(indicator_mat[j])
                         if abs(tmp - 1) < 1e-2:
                             break
@@ -113,12 +113,75 @@ def bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat):
     return obj
 
 
+def objective_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat):
+    N, J = tau_mat.shape
+    _, K, m = h_mat.shape
+    obj = 0
+    for n in range(N):
+        for j in range(J):
+            obj_k = 0
+            for k in range(K):
+                h_vec = numpy.mat(h_mat[n, k, :]).reshape((m, 1))
+                obj_k += indicator_mat[j, k] * b_mat[n, j] * numpy.sum(numpy.dot(a_list[j].T, h_vec))
+            obj += tau_mat[n, j] * (obj_k - 1) ** 2
+    return obj
+
+
 def round_indicator_mat(indicator_mat):
     J, K = indicator_mat.shape
     for j in range(J):
         idx = numpy.argmax(indicator_mat[j])
         for k in range(K):
             indicator_mat[j, k] = 1 if k == idx else 0
+    return indicator_mat
+
+
+def assignment_index(indicator_mat):
+    J, K = indicator_mat.shape
+    for j in range(J):
+        if sum(indicator_mat[j]) == 0:
+            return j
+    return J
+
+
+def round_indicator_mat_v2(idx, c_mat, subcarrier_set):
+    J, K = c_mat.shape
+    augmented_c_mat = numpy.zeros((K, K))
+    for j in range(J):
+        for k in range(K):
+            augmented_c_mat[j, k] = c_mat[j, k]
+    augmented_indicator_mat = numpy.zeros((K, K))
+    for j in range(idx):
+        for k in range(K):
+            augmented_indicator_mat[j, k] = c_mat[j, k]
+    visited_mat = numpy.zeros((K, K))
+
+    while assignment_index(augmented_indicator_mat) < K:
+        unassigned_j = assignment_index(augmented_indicator_mat)
+        idx_list = numpy.argsort(augmented_c_mat[unassigned_j])
+        idx = 0
+        for k in range(K):
+            if visited_mat[unassigned_j, idx_list[K - k - 1]] == 0 and idx_list[K - k - 1] in subcarrier_set:
+                idx = idx_list[K - k - 1]
+                visited_mat[unassigned_j, idx] = 1
+                break
+
+        if sum(augmented_indicator_mat[:, idx]) == 0:
+            augmented_indicator_mat[unassigned_j, idx] = 1
+        else:
+            pre_j = 0
+            for j in range(K):
+                if augmented_indicator_mat[j, idx] == 1:
+                    pre_j = j
+                    break
+            if augmented_c_mat[unassigned_j, idx] > augmented_c_mat[pre_j, idx]:
+                augmented_indicator_mat[unassigned_j, idx] = 1
+                augmented_indicator_mat[pre_j, idx] = 0
+
+    indicator_mat = numpy.zeros((J, K))
+    for j in range(J):
+        for k in range(K):
+            indicator_mat[j, k] = augmented_indicator_mat[j, k]
     return indicator_mat
 
 
@@ -162,7 +225,9 @@ def exhaustive_search(tau_mat, h_mat, a_list, b_mat):
         idx = node.get_idx()
         if idx == J:
             indicator_mat = node.get_indicator_mat()
-            obj = bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
+            # obj = bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
+            obj = objective_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
+            print(obj)
             if obj < bound:
                 bound = obj
                 current_optimal = indicator_mat
@@ -174,11 +239,12 @@ def exhaustive_search(tau_mat, h_mat, a_list, b_mat):
                     indicator_mat[idx, k] = 1 if selected_k == k else 0
                 tmp_subcarrier_set = subcarrier_set.copy()
                 tmp_subcarrier_set.discard(selected_k)
-                node_list.append(Node(idx+1, tmp_subcarrier_set, indicator_mat, 0, 0))
+                node_list.append(Node(idx + 1, tmp_subcarrier_set, indicator_mat, 0, 0))
     return current_optimal
 
 
-def BnB_subcarrier_allocation(tau_mat, h_mat, a_list, b_mat, alpha_mat, square_alpha_mat, max_iter=100, tol=1e-3):
+def BnB_subcarrier_allocation(tau_mat, h_mat, a_list, b_mat, stored_value_mat, stored_sqaure_value_mat, max_iter=100,
+                              tol=1e-3):
     N, J = tau_mat.shape
     _, K, m = h_mat.shape
     global_lower_bound = -1e6
@@ -191,12 +257,16 @@ def BnB_subcarrier_allocation(tau_mat, h_mat, a_list, b_mat, alpha_mat, square_a
     for k in range(K):
         subcarrier_set.add(k)
     indicator_mat = numpy.zeros((J, K))
-    indicator_mat = partial_subcarrier_optimization(0, subcarrier_set, tau_mat, h_mat, alpha_mat, square_alpha_mat,
+    indicator_mat = partial_subcarrier_optimization(0, subcarrier_set, tau_mat, h_mat, stored_value_mat,
+                                                    stored_sqaure_value_mat,
                                                     indicator_mat)
-    global_lower_bound = bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
-    discrete_indicator_mat = round_indicator_mat(indicator_mat.copy())
-    if check_indicator_constraints(discrete_indicator_mat):
-        global_upper_bound = bound_calculation(tau_mat, h_mat, a_list, discrete_indicator_mat, b_mat)
+    # global_lower_bound = bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
+    global_lower_bound = objective_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
+    # discrete_indicator_mat = round_indicator_mat(indicator_mat.copy())
+    # discrete_indicator_mat = round_indicator_mat_v2(0, indicator_mat.copy(), subcarrier_set)
+    # if check_indicator_constraints(discrete_indicator_mat):
+    #     global_upper_bound = bound_calculation(tau_mat, h_mat, a_list, discrete_indicator_mat, b_mat)
+    # global_upper_bound = bound_calculation(tau_mat, h_mat, a_list, discrete_indicator_mat, b_mat)
 
     node_list.append(Node(0, subcarrier_set, indicator_mat, global_lower_bound, global_upper_bound))
     while global_upper_bound - global_lower_bound > tol:
@@ -204,11 +274,11 @@ def BnB_subcarrier_allocation(tau_mat, h_mat, a_list, b_mat, alpha_mat, square_a
         idx = node.get_idx()
         subcarrier_set = node.get_subcarrier_set()
 
+        # print('indicator mat', node.get_indicator_mat())
         print('global upper bound ' + str(global_upper_bound))
         print('global lower bound ' + str(global_lower_bound))
         print('idx ' + str(idx))
         print('subcarrier set: ', subcarrier_set)
-        print('indicator mat', node.get_indicator_mat())
 
         for selected_k in list(subcarrier_set):
             indicator_mat = node.get_indicator_mat()
@@ -218,23 +288,34 @@ def BnB_subcarrier_allocation(tau_mat, h_mat, a_list, b_mat, alpha_mat, square_a
             tmp_subcarrier_set = subcarrier_set.copy()
             if idx < J - 1:
                 tmp_subcarrier_set.discard(selected_k)
-                indicator_mat = partial_subcarrier_optimization(idx + 1, tmp_subcarrier_set, tau_mat, h_mat, alpha_mat,
-                                                                square_alpha_mat, indicator_mat)
-            lower_bound = bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
+                indicator_mat = partial_subcarrier_optimization(idx + 1, tmp_subcarrier_set, tau_mat, h_mat,
+                                                                stored_value_mat, stored_sqaure_value_mat,
+                                                                indicator_mat)
+            # lower_bound = bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
+            lower_bound = objective_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)
             if lower_bound < global_upper_bound:
                 # global_lower_bound = lower_bound if lower_bound < global_lower_bound else global_lower_bound
-                discrete_indicator_mat = round_indicator_mat(indicator_mat.copy())
+                # discrete_indicator_mat = round_indicator_mat(indicator_mat.copy())
+                discrete_indicator_mat = round_indicator_mat_v2(idx + 1, indicator_mat.copy(), tmp_subcarrier_set)
                 upper_bound = global_upper_bound
-                if check_indicator_constraints(discrete_indicator_mat):
-                    upper_bound = bound_calculation(tau_mat, h_mat, a_list, discrete_indicator_mat, b_mat)
-                    if upper_bound < global_upper_bound:
-                        global_upper_bound = upper_bound
-                        optimal_indicator_mat = discrete_indicator_mat
+                # if check_indicator_constraints(discrete_indicator_mat):
+                #     upper_bound = bound_calculation(tau_mat, h_mat, a_list, discrete_indicator_mat, b_mat)
+                #     if upper_bound < global_upper_bound:
+                #         global_upper_bound = upper_bound
+                #         optimal_indicator_mat = discrete_indicator_mat
+                # upper_bound = bound_calculation(tau_mat, h_mat, a_list, discrete_indicator_mat, b_mat)
+                upper_bound = objective_calculation(tau_mat, h_mat, a_list, discrete_indicator_mat, b_mat)
+                if upper_bound < global_upper_bound:
+                    global_upper_bound = upper_bound
+                    optimal_indicator_mat = discrete_indicator_mat
+                    # print('update optimal indicator')
                 if idx < J - 1:
                     node_list.append(Node(idx + 1, tmp_subcarrier_set, indicator_mat, lower_bound, upper_bound))
         if node_list:
             node_list.sort(key=functools.cmp_to_key(node_cmp))
             global_lower_bound = node_list[0].get_lower_bound()
+            for node in node_list:
+                print(node.get_lower_bound())
         else:
             break
     return optimal_indicator_mat
@@ -249,6 +330,21 @@ if __name__ == '__main__':
     P = 10
     tau_mat = numpy.zeros((N, J))
     h_mat = abs(numpy.random.randn(N, K, m))
+
+    for n in range(N):
+        subcarrier_scale_list = numpy.zeros(K)
+        step = int(K / 4)
+        subcarrier_scale_list[0:step] = 0.1 * numpy.random.random_sample(step) + 0.1
+        # subcarrier_scale_list[int(K / 4):] = 5 * numpy.random.random_sample(int(K / 2)) + 5
+        # subcarrier_scale_list[int(K / 4):] = 1
+        subcarrier_scale_list[step:2 * step] = 0.01 * numpy.random.random_sample(step) + 0.01
+        subcarrier_scale_list[2 * step:3 * step] = 1 * numpy.random.random_sample(step) + 1
+        subcarrier_scale_list[3 * step:] = 1
+        subcarrier_scale_list = subcarrier_scale_list[numpy.random.permutation(K)]
+        tmp_subcarrier_scale_list = subcarrier_scale_list[numpy.random.permutation(K)]
+        for k in range(K):
+            h_mat[n, k] = subcarrier_scale_list[k] * h_mat[n, k]
+
     for n in range(N):
         for j in range(J):
             tau_mat[n, j] = numpy.random.uniform()
@@ -277,7 +373,13 @@ if __name__ == '__main__':
                 alpha_mat[n, j, k] = b_mat[n, j] * numpy.sum(
                     numpy.dot(a_list[j].T, numpy.mat(h_mat[n, k, :]).reshape((m, 1))))
                 square_alpha_mat[n, j, k] = alpha_mat[n, j, k] ** 2
-    indicator_mat = BnB_subcarrier_allocation(tau_mat, h_mat, a_list, b_mat, alpha_mat, square_alpha_mat)
+    stored_value_mat = numpy.zeros((J, K))
+    stored_sqaure_value_mat = numpy.zeros((J, K))
+    for j in range(J):
+        for k in range(K):
+            stored_value_mat[j, k] = 2 * numpy.sum(numpy.multiply(tau_mat[:, j], alpha_mat[:, j, k]))
+            stored_sqaure_value_mat[j, k] = 2 * numpy.sum(numpy.multiply(tau_mat[:, j], square_alpha_mat[:, j, k]))
+    indicator_mat = BnB_subcarrier_allocation(tau_mat, h_mat, a_list, b_mat, stored_value_mat, stored_sqaure_value_mat)
     print('BnB subcarrier allocation: ' + str(bound_calculation(tau_mat, h_mat, a_list, indicator_mat, b_mat)))
 
     indicator_mat = exhaustive_search(tau_mat, h_mat, a_list, b_mat)
